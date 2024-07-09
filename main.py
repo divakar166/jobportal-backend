2ffc4e450d2fe1d5b5b8b1fb001c1038bb331687
Divakar Singh <ds299555@gmail.com>
2024-07-09 10:45:25 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from routes.developers import dev_router
 from routes.companies import company_router
 from routes.jobs import job_router
@@ -19,10 +20,27 @@ app.include_router(dev_router, prefix='/developers')
 app.include_router(job_router, prefix='/jobs')
 app.include_router(company_router, prefix='/companies')
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def index():
-    {"message": "Welcome to Connect - JobPortal Backend"}
+    html_content = """
+    <html>
+        <head>
+            <title>Connect - JobPortal Backend</title>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+                *{
+                    font-family:"Inter";
+                }
+            </style>
+        </head>
+        <body>
+            <h1 style="text-align: center; margin-top: 20%;color:#a855f7;font-weight:900;">Welcome to Connect - JobPortal Backend</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
 
-import uvicorn
+
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app)
